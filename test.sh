2796e12f44fb9c6d75139d4ab2d1ae6ef80130f3
AleksandrTolstoy <st063510@student.spbu.ru>
2022-06-04 19:16:10 +0300

    added test.sh for wget books

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget http://www.gutenberg.org/files/20417/20417.txt
+wget http://www.gutenberg.org/files/5000/5000-8.txt
+wget http://www.gutenberg.org/files/4300/4300-0.txt
+wget http://www.gutenberg.org/files/972/972.txt 
